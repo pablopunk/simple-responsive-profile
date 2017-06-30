7aabc8ae8cceb517a6e697481bf1a0101c8d2dc5
Pablo Varela <pablovarela182@gmail.com>
2017-06-29 20:35:29 -0400

    First commit

---
+++ scripts/index.js
@@ -0,0 +1,21 @@
+
+function listItemClicked(event) {
+  var link = event.target.querySelector('a').href
+  console.log(link)// window.location.href = link
+}
+
+document.querySelectorAll('.link-list li').forEach(function (el) {
+  el.addEventListener('click', listItemClicked)
+})
+
+console.log(`%c
+ _______________________________________________
+| It was so easy to create this website using   |
+| codepen and Flexbox, that I had time to teach |
+| this cow how to talk                          |
+\`-----------------------------------------------´
+        \\   ^__^
+         \\  (oo)\\_______
+            (__)\\       )\\/\\
+                ||----w |
+                ||     ||`, "font-family:monospace")
